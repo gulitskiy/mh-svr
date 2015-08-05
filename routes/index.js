848d1ec9fbf7e58e583e9ff1ec6e0d0a49c40349
Alexandr Gulitskiy <alexandr.gulitskiy@mail.ru>
2015-08-05 22:15:59 +0600

    новый поисковой запрос

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 module.exports = function (routes, es) {
 	require('./news')(routes, es);
+	require('./search')(routes, es);
 };
--- routes/search.js
+++ routes/search.js
@@ -1,44 +1,82 @@
 var logger = require('../utils/logger');
+var dateFormat = require('dateformat');
+
+var formatDate = 'yyyy-mm-dd HH:MM:ss';
 
 module.exports = function (routes, es) {
 	
 	// Поиск по ключевым словам
-	routes.post('/search', function(req, res) {
+	routes.get('/search', function(req, res) {
+	
+		var query = null, dateFrom = null;
 		
-		var result = [];
-		var keywords = req.body;
+		if(!req.query.q) {
+			res.json({'status': 'error', 'message': 'q param is required'});
+			return;
+		}
 		
-		var dateFrom = new Date('2015-01-01');
+		var query = req.query.q;
+		var dateFrom = new Date(req.query.dateFrom);
+		var keywords = query.split(' ');
 		
-		es.search({
-			index: '_all',
-			body: {
-				'query': {
-					'filtered': {
-						'query': {
-							'match': {
-								'text': {
-									'query': keywords,
-									'fuzziness': 'AUTO',
-									'operator': 'and'
-								}
-							}
-						},
-						'filter': {
-							'range': {
-								'pubDate': {
-									'gte': '2015-08-03'
+		var clauses = [];
+		
+		for(var i = 0; i < keywords.length; i++) {
+			if(keywords[i] && keywords[i].length > 0) {
+				var clause = {
+					'span_multi': {
+						'match': {
+							'fuzzy': {
+								'fullText': {
+									"fuzziness": "AUTO",
+									"value": keywords[i]
 								}
 							}
 						}
+					}
+				};
+				clauses.push(clause);
+			}
+		}
+		
+		var esQuery = {
+			'query': {
+				'filtered': {
+					'query': {
+						'span_near': {
+							'clauses': clauses,
+							'slop' : 2,
+							'in_order' : true,
+							//'boost' : 2
+						}
 					},
-					'highlight': {
-						'fields': {
-							'text': {}
+					'filter': {
+						'range': {
+							'pubDate': {
+								'gte': dateFormat(dateFrom, formatDate)
+							}
 						}
 					}
 				}
-			}
+			},
+			'highlight' : {
+				'pre_tags' : ['<b>'],
+				'post_tags' : ['</b>'],
+				'order' : 'score',
+				'fields' : {
+					'fullText' : {
+						'fragment_size' : 150,
+						'number_of_fragments' : 3
+					}
+				}
+			}			
+		};
+		
+		//res.json(esQuery);
+		
+		es.search({
+			index: '_all',
+			body: esQuery
 		}, function (error, response) {
 			res.json(response);
 		});
@@ -50,7 +88,3 @@ module.exports = function (routes, es) {
 /*
 	['Мажилис парламента', 'Председатель мажилиса']
 */
-
-function searchNews(keyword, result) {
-	var query = [];
-}
